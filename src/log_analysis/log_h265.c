a24a84c3ec7548472d000af3f6528c9bacf663eb
SUENAGA Hiroki <hsuenaga@iij.ad.jp>
2025-06-26 16:03:17 +0900

    cleanup

--- src/log_analysis/log_h265.c
+++ src/log_analysis/log_h265.c
@@ -28,17 +28,32 @@ init_player(struct wfb_gst_context *ctx)
 	wfb_gst_thread_start(ctx);
 }
 
-void
-play_h265(struct log_store *ls)
+static void
+init_writer(struct wfb_gst_context *ctx, const char *file, bool enc)
+{
+	assert(ctx);
+
+	if (ctx->initialized)
+		return;
+	wfb_gst_context_init(ctx, file, enc);
+	wfb_gst_thread_start(ctx);
+}
+
+static void
+send_data(struct wfb_gst_context *ctx, struct log_store *ls)
 {
 	struct timespec epoch, elapsed;
 	struct log_data_kv *kv;
 	struct log_data_v *v;
+	bool emulate_tick = true;
 
-	init_player(&player_ctx);
+	if (ctx->file && !ctx->enc)
+		emulate_tick = false;
 
-	clock_gettime(CLOCK_MONOTONIC, &epoch);
-	timespecclear(&elapsed);
+	if (emulate_tick) {
+		clock_gettime(CLOCK_MONOTONIC, &epoch);
+		timespecclear(&elapsed);
+	}
 	TAILQ_FOREACH(kv, &ls->kvh, chain) {
 		int8_t dbm = INT8_MIN;
 		TAILQ_FOREACH(v, &kv->vh, chain) {
@@ -52,11 +67,15 @@ play_h265(struct log_store *ls)
 			if (v->filtered)
 				continue;
 			for (;;) {
-				clock_gettime(CLOCK_MONOTONIC, &elapsed);
-				timespecsub(&elapsed, &epoch, &elapsed);
-				if (timespeccmp(&elapsed, &v->ts, <)) {
-					usleep(1);
-					continue;
+				if (emulate_tick) {
+					clock_gettime(CLOCK_MONOTONIC,
+					    &elapsed);
+					timespecsub(&elapsed, &epoch,
+					    &elapsed);
+					if (timespeccmp(&elapsed, &v->ts, <)) {
+						usleep(1);
+						continue;
+					}
 				}
 				wfb_gst_add_dbm(dbm, &player_ctx);
 				wfb_gst_write(&v->ts,
@@ -69,75 +88,38 @@ play_h265(struct log_store *ls)
 }
 
 void
+play_h265(struct log_store *ls)
+{
+	init_player(&player_ctx);
+	send_data(&player_ctx, ls);
+}
+
+void
 write_mp4(const char *file, struct log_store *ls)
 {
 	struct wfb_gst_context ctx;
-	struct timespec epoch, elapsed;
-	struct log_data_kv *kv;
-	struct log_data_v *v;
 
 	if (file == NULL) {
 		p_err("Please specify output file name.\n");
 		exit(0);
 	}
 
-	wfb_gst_context_init(&ctx, file, false);
-	wfb_gst_thread_start(&ctx);
-
-	TAILQ_FOREACH(kv, &ls->kvh, chain) {
-		TAILQ_FOREACH(v, &kv->vh, chain) {
-			if (v->type != FRAME_TYPE_DECODE)
-				continue;
-			if (v->filtered)
-				continue;
-			wfb_gst_write(&v->ts, v->buf, v->size, &ctx);
-		}
-	}
-	wfb_gst_eos(&ctx);
+	memset(&ctx, 0, sizeof(ctx));
+	init_writer(&ctx, file, false);
+	send_data(&ctx, ls);
 }
 
 void
 write_mp4_enc(const char *file, struct log_store *ls)
 {
 	struct wfb_gst_context ctx;
-	struct timespec epoch, elapsed;
-	struct log_data_kv *kv;
-	struct log_data_v *v;
 
 	if (file == NULL) {
 		p_err("Please specify output file name.\n");
 		exit(0);
 	}
 
-	wfb_gst_context_init(&ctx, file, true);
-	wfb_gst_thread_start(&ctx);
-
-	clock_gettime(CLOCK_MONOTONIC, &epoch);
-	timespecclear(&elapsed);
-	TAILQ_FOREACH(kv, &ls->kvh, chain) {
-		int8_t dbm = INT8_MIN;
-		TAILQ_FOREACH(v, &kv->vh, chain) {
-			if (v->type == FRAME_TYPE_INET6) {
-				if (v->dbm > dbm) 
-					dbm = v->dbm;
-				continue;
-			}
-			if (v->type != FRAME_TYPE_DECODE)
-				continue;
-			if (v->filtered)
-				continue;
-			for (;;) {
-				clock_gettime(CLOCK_MONOTONIC, &elapsed);
-				timespecsub(&elapsed, &epoch, &elapsed);
-				if (timespeccmp(&elapsed, &v->ts, <)) {
-					usleep(1);
-					continue;
-				}
-				wfb_gst_add_dbm(dbm, &ctx);
-				wfb_gst_write(&v->ts, v->buf, v->size, &ctx);
-				break;
-			}
-		}
-	}
-	wfb_gst_eos(&ctx);
+	memset(&ctx, 0, sizeof(ctx));
+	init_writer(&ctx, file, true);
+	send_data(&ctx, ls);
 }
